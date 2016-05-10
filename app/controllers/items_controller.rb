18a2ee315614a0c9d7965ae038b282eb8fb5ba22
Kristina Chang <kristinachang@users.noreply.github.com>
2016-05-09 22:45:38 -0700

    update items_controller, views

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -13,6 +13,71 @@ class ItemsController < ApplicationController
 		@daily = Daily.find(params[:daily_id])
 		@item = Item.new
 
+	 #    Add the following when implementing search function on /new.
+	 #    if params[:q]
+	 #      search = params[:q]
+	 #      if search
+	 #        resp = Typhoeus.get(
+	 #          "http://api.nal.usda.gov/usda/ndb/search",
+	 #          params: {
+	 #          format: "json",
+	 #          q: search,
+	 #          sort: "n",
+	 #          max: 30,
+	 #          offset: 0,
+	 #          api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+	 #          }
+	 #        )
+	 #        @foods = JSON.parse(resp.body)["list"]["item"]
+	 #      else 
+	 #        @foods = []
+	 #      end
+	 #    end		
+	end
+
+	def create
+		@daily=Daily.find(params[:daily_id])
+		@item = @daily.items.new(item_params)
+
+	 #    Add the following when implementing search function on /new.
+	 #    if params[:ndbno].present?
+	 #      resp = Typhoeus.get(
+	 #        "http://api.nal.usda.gov/usda/ndb/reports",
+	 #        params: {
+	 #        format: "json",
+	 #        ndbno: params[:ndbno],
+	 #        type: "b",
+	 #        api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+	 #        }
+	 #      )
+	 #      @food = JSON.parse(resp.body)["report"]
+
+	 #      # @item = @daily.items.new (this line would create a new item instead of updating the current one)
+
+	 #      @item.name = @food["food"]["name"]
+	 #      @item.ndbno = params[:ndbno]
+	 #      @item.kcal = @food["food"]["nutrients"][1]["measures"][0]["value"]
+	 #      @item.protein = @food["food"]["nutrients"][2]["measures"][0]["value"]
+	 #      @item.fat = @food["food"]["nutrients"][3]["measures"][0]["value"]
+	 #      @item.carb = @food["food"]["nutrients"][4]["measures"][0]["value"]
+
+	 #      @item.save
+	      
+	 #      redirect_to user_daily_item_path(current_user, @daily, @item)
+	 #    end
+
+		if @item.save
+			redirect_to user_daily_item_path(current_user, @daily, @item)
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@user = current_user
+	    @daily = Daily.find(params[:daily_id])
+	    @item = Item.find(params[:id])
+
 	    if params[:q]
 	      search = params[:q]
 	      if search
@@ -27,71 +92,11 @@ class ItemsController < ApplicationController
 	          api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
 	          }
 	        )
-	        @items = JSON.parse(resp.body)["list"]["item"]
+	        @foods = JSON.parse(resp.body)["list"]["item"]
 	      else 
-	        @items = []
+	        @foods = []
 	      end
 	    end
-	    @user = current_user
-	    # find the article we are interested in
-	    # pass in params[:id] to get :id parameter from request
-	    # @ is an instance variable to hold a reference to the entry object
-	    @daily = Daily.find(params[:daily_id])
-	end
-
-	def create
-		@daily=Daily.find(params[:daily_id])
-		@item = @daily.items.new(item_params)
-
-		if params[:ndbno].present?
-	      resp = Typhoeus.get(
-	        "http://api.nal.usda.gov/usda/ndb/reports",
-	        params: {
-	        format: "json",
-	        ndbno: params[:ndbno],
-	        type: "b",
-	        api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
-	        }
-	      )
-	      @food = JSON.parse(resp.body)["report"]
-
-	      @item = @daily.items.new(item_params)
-
-	      @item.name = @food["food"]["name"]
-	      @item.ndbno = params[:ndbno]
-	      @item.kcal = @food["food"]["nutrients"][1]["measures"][0]["value"]
-	      @item.protein = @food["food"]["nutrients"][2]["measures"][0]["value"]
-	      @item.fat = @food["food"]["nutrients"][3]["measures"][0]["value"]
-	      @item.carb = @food["food"]["nutrients"][4]["measures"][0]["value"]
-
-	      @item.save
-	      
-	      redirect_to @entry  
-	    end
-
-		if @item.save
-			redirect_to user_daily_item_path(current_user, @item.daily, @item)
-		else
-			render 'new'
-		end
-	end
-
-	def show
-	    # report = params[:q]
-	    # if report 
-	    #   resp = Typhoeus.get(
-	    #     "http://api.nal.usda.gov/usda/ndb/reports",
-	    #     params: {
-	    #     format: "json",
-	    #     ndbno: report,
-	    #     type: "b",
-	    #     api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
-	    #     }
-	    #   )
-	    #   @food = JSON.parse(resp.body)["report"]
-	    # else
-	    #   @food = {}
-	    # end
 	end
 
 	def edit
@@ -103,55 +108,53 @@ class ItemsController < ApplicationController
 	def update
 		@user = current_user
 		@daily = Daily.find(params[:daily_id])
-		@item = @daily.items.new(item_params)
+		@item = Item.find(params[:id])
 
-	    # if params[:ndbno].present?
-	    #   resp = Typhoeus.get(
-	    #     "http://api.nal.usda.gov/usda/ndb/reports",
-	    #     params: {
-	    #     format: "json",
-	    #     ndbno: params[:ndbno],
-	    #     type: "b",
-	    #     api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
-	    #     }
-	    #   )
-	    #   @food = JSON.parse(resp.body)["report"]
+	    if params[:ndbno].present?
+	      resp = Typhoeus.get(
+	        "http://api.nal.usda.gov/usda/ndb/reports",
+	        params: {
+	        format: "json",
+	        ndbno: params[:ndbno],
+	        type: "b",
+	        api_key: "bT0Q1R0Js9aaOsjTR9ro6Oax1y21Wg2J8fmr74Vc"
+	        }
+	      )
+	      @food = JSON.parse(resp.body)["report"]
 
-	    #   @item = @daily.items.new(item_params)
+	      # @item = @daily.items.new (this line would create a new item instead of updating the current one)
 
-	    #   @item.name = @food["food"]["name"]
-	    #   @item.ndbno = params[:ndbno]
-	    #   @item.kcal = @food["food"]["nutrients"][1]["measures"][0]["value"]
-	    #   @item.protein = @food["food"]["nutrients"][2]["measures"][0]["value"]
-	    #   @item.fat = @food["food"]["nutrients"][3]["measures"][0]["value"]
-	    #   @item.carb = @food["food"]["nutrients"][4]["measures"][0]["value"]
+	      @item.name = @food["food"]["name"]
+	      @item.ndbno = params[:ndbno]
+	      @item.kcal = @food["food"]["nutrients"][1]["measures"][0]["value"]
+	      @item.protein = @food["food"]["nutrients"][2]["measures"][0]["value"]
+	      @item.fat = @food["food"]["nutrients"][3]["measures"][0]["value"]
+	      @item.carb = @food["food"]["nutrients"][4]["measures"][0]["value"]
 
-	    #   @item.save
+	      @item.save
 	      
-	    #   redirect_to @entry  
-	    # end
+	      redirect_to user_daily_item_path(current_user, @daily, @item)
+	    end
 
-	    # unless params[:ndbno].present?
-	    #   if @daily.update(daily_params)
-	    #     # redirect back to the show page with the way our routes are nested
-	    #     # @entry.log can also be params[:log_id]
-	    #     redirect_to user_daily_path(current_user, @entry)
-	    #   else
-	    #     render 'edit'
-	    #   end
-	    # end
+	    unless params[:ndbno].present?
+	      if @item.update(item_params)
+	        redirect_to user_daily_item_path(current_user, @daily, @item)
+	      else
+	        render 'edit'
+	      end
+	    end
 	end
 
 	def destroy
 		@item = Item.find(params[:id])
 		@item.destroy
 
-		redirect_to user_daily_path(current_user, @daily)
+		redirect_to user_dailies_path(current_user, @daily)
 	end
 
 	private
 		def item_params
-			params.require(:item).permit(:time, :name, :ndbno, :protein, :fat, :carb, :kcal, :unit, :mood, :notes)
+			params.require(:item).permit(:time, :name, :mood, :notes)
 		end
 	
 end
